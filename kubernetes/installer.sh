b1b43b896526d0c933d89ff71bc3f654ea33563e
mzanella <zanna0150@gmail.com>
2018-07-18 10:29:21 +0200

    Add installer

---
+++ kubernetes/installer.sh
@@ -0,0 +1,10 @@
+branch="master"
+while getopts ":b:" opt; do
+        case $opt in
+            b)
+                branch="$OPTARG"
+                ;;
+        esac
+done
+
+bash <(curl -s https://raw.githubusercontent.com/Augugrumi/init-script/$branch/kubernetes/on_openstack_cli.sh) $@
