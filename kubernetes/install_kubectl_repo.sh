188cbfbef464f4592b94149e7b8b392f1001429c
Davide Polonio <poloniodavide@gmail.com>
2018-07-18 16:54:01 +0200

    Add logging

--- kubernetes/install_kubectl_repo.sh
+++ kubernetes/install_kubectl_repo.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+. <(curl -s https://raw.githubusercontent.com/Polpetta/minibashlib/master/minibashlib.sh)
+
 # require ips file in /tmp/ with the array of ips of the hosts
 read -a ips <<< $(cat $1)
- 
+
+mb_load "logging"
 toWait=()
 echo ${ips[@]}
+msg info "Preparing to bootstrap..."
 for i in ${ips[@]}
 do
     # update machiens and install necessary sw
@@ -12,6 +16,8 @@ do
     # store all PID of process in order to wait until all machine are ready and rebooted
     toWait+=($!)
 done
+
+msg info "Done bootstrapping"
 echo ${toWait[@]}
 
 # code for waiting for the machine
@@ -19,3 +25,5 @@ for i in ${toWait[@]}
 do
     wait $i
 done
+
+msg info "All nodes are up and running, exiting install_kubectl_repo.sh"
