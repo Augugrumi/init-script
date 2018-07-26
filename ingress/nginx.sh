e1f8b2d5db002ab593e39e0406b079cb8e4a36da
mzanella <mzanella@users.noreply.github.com>
2018-07-26 14:10:46 +0200

    Update nginx.sh

--- ingress/nginx.sh
+++ ingress/nginx.sh
@@ -4,4 +4,5 @@ git clone https://github.com/nginxinc/kubernetes-ingress.git --depth 1
 helm install --name nginxcontroller . --set controller.replicaCount=2,controller.service.type=NodePort,controller.service.externalTrafficPolicy=Cluster
 
 echo "To install your services run
-helm install <service-release> --name <your-service-name> --set serviceType=NodePort,nodePorts.http=<your-port>"
+helm install <service-release> --name <your-service-name> --set serviceType=NodePort,nodePorts.http=<your-port>
+"
